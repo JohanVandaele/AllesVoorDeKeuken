9f7c790443096ce7259bef67ec5b025ba0bd7b9e
“JohanVandaele” <johanvandaele@hotmail.com>
2014-08-21 09:55:28 +0200

    Commit   [-7]

---
+++ src/main/java/be/vdab/servlets/artikels/ZoekenServlet.java
@@ -0,0 +1,40 @@
+package be.vdab.servlets.artikels;
+
+import java.io.IOException;
+import java.util.Collections;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import be.vdab.dao.ArtikelDAO;
+
+// enkele imports ...
+
+@WebServlet("/artikels/zoekenopnummer.htm")
+public class ZoekenServlet extends HttpServlet
+{
+	private static final long serialVersionUID = 1L;
+	private static final String VIEW = "/WEB-INF/JSP/artikels/zoeken.jsp";
+	private final transient ArtikelDAO artikelDAO = new ArtikelDAO();
+	
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)throws ServletException, IOException
+	{
+		if (request.getQueryString() != null)
+		{
+			try
+			{
+				request.setAttribute("artikel",artikelDAO.read(Long.parseLong(request.getParameter("id"))));
+			}
+			catch (NumberFormatException ex)
+			{
+				request.setAttribute("fouten",Collections.singletonMap("id", "tik een getal"));
+			}
+		}
+		
+		request.getRequestDispatcher(VIEW).forward(request, response);
+	}
+}
