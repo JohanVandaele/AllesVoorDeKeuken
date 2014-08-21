ba76efa63429f52da24f4031fe649ed3587c0858
“JohanVandaele” <johanvandaele@hotmail.com>
2014-08-21 15:20:40 +0200

    Commit [-12]

--- src/main/java/be/vdab/dao/ArtikelDAO.java
+++ src/main/java/be/vdab/dao/ArtikelDAO.java
@@ -1,39 +1,16 @@
 package be.vdab.dao;
 
-import javax.persistence.EntityManager;
-
 import be.vdab.entities.Artikel;
-//import be.vdab.filters.JPAFilter;
 
-// enkele imports ...
-
-public class ArtikelDAO
+public class ArtikelDAO extends AbstractDAO
 {
-//	public Artikel read(long id)
-//	{
-//		// Je vraagt een EntityManager aan de servlet filter JPAFilter.
-//		EntityManager entityManager = JPAFilter.getEntityManager();
-//
-//		try
-//		{
-//			// Je zoekt een Docent entity op de primary key met de find method.
-//			// De 1° parameter is het type van de op te zoeken entity
-//			// De 2° parameter is de primary key waarde van de op te zoeken entity
-//			return entityManager.find(Artikel.class, id);
-//		}
-//		finally
-//		{
-//			entityManager.close();
-//		}
-//	}
-
-	public Artikel read(long id, EntityManager entityManager)
+	public Artikel read(long id)
 	{
-		return entityManager.find(Artikel.class, id);
+		return getEntityManager().find(Artikel.class, id);
 	}
 	
-	public void create(Artikel artikel, EntityManager entityManager)
+	public void create(Artikel artikel)
 	{
-		entityManager.persist(artikel);
+		getEntityManager().persist(artikel);
 	}	
 }
--- src/main/java/be/vdab/filters/JPAFilter.java
+++ src/main/java/be/vdab/filters/JPAFilter.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 
 
 
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
@@ -16,42 +17,52 @@ import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.annotation.WebFilter;
 
-// enkele imports ...
-
 @WebFilter("*.htm")
 public class JPAFilter implements Filter
 {
-	// Deze variabele is de EnityManagerFactory. Een static variabele blijft in RAM gedurende de levensduur van de website.
-	// Je maakt een EntityManagerFactory object met de static Persistence method createEntityManagerFactory. Je geeft de name mee van het element persistence-unit uit persistence.xml.
 	private static final EntityManagerFactory entityManagerFactory=Persistence.createEntityManagerFactory("allesvoordekeuken");
+	private static final ThreadLocal<EntityManager> entityManagers=new ThreadLocal<>();
 	
 	@Override
 	public void init(FilterConfig filterConfig) throws ServletException
 	{
 		ServletContext context = filterConfig.getServletContext();
-		
-		// Elke JSP zal in ${contextPath} de context path van de website kunnen lezen. Die bevat bij ons de String /fietsacademy
 		context.setAttribute("contextPath", context.getContextPath());
 	}
 
 	public static EntityManager getEntityManager()
 	{
-		return entityManagerFactory.createEntityManager();
+		return entityManagers.get();
 	}	
 	
 	@Override
 	public void doFilter(ServletRequest request, ServletResponse response,FilterChain chain) throws ServletException, IOException
 	{
-		// Je geeft aan dat request parameters uitgedrukt zijn in UTF-8. Je verwerkt zo request parameters met ‘vreemde tekens’ op een correcte manier.
-		request.setCharacterEncoding("UTF-8");
-		chain.doFilter(request, response);
+		entityManagers.set(entityManagerFactory.createEntityManager());
+		
+		try
+		{
+			request.setCharacterEncoding("UTF-8");
+			// Je geeft de request door aan de servlet waarvoor hij bestemd is.
+			chain.doFilter(request, response);
+		}
+		finally
+		{
+			EntityManager entityManager = entityManagers.get();
+			
+			if (entityManager.getTransaction().isActive())
+			{
+				entityManager.getTransaction().rollback();
+			}
+			
+			entityManager.close();
+			entityManagers.remove();
+		}	
 	}
 	
-	// De webserver roept deze method op bij het stoppen van de website.
 	@Override
 	public void destroy()
 	{
-		// Je sluit de EntityManagerFactory.
 		entityManagerFactory.close();
 	}
 }
